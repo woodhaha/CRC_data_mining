5d414443bf5e052cec300600fd50a4f2444c0117
ZhouZhuha <woodhaha@gmail.com>
2019-06-25 14:24:49 +0800

    Update CaliPlot.R

--- CaliPlot.R
+++ CaliPlot.R
@@ -1,12 +1,13 @@
 CaliPlot = function(dataset) { 
 	lift <- data.frame(group = dataset$group)
 	lift$logisreg <- predict(logisreg, dataset, type = "prob")[, "NonTumor"]
-  lift$elastnet <- predict(elastnet, dataset, type = "prob")[,"NonTumor"]
- lift$RF <- predict(RF, dataset, type = "prob")[,"NonTumor"]
- lift$SVM <- predict(SVM, dataset, type = "prob")[,"NonTumor"]
- lift$NNET <- predict(NNET, dataset, type = "prob")[,"NonTumor"]
- cal_obj <- calibration(group ~ logisreg + elastnet + RF+SVM+NNET, data = lift,cuts = 10)
-
+        lift$elastnet <- predict(elastnet, dataset, type = "prob")[,"NonTumor"]
+        lift$RF <- predict(RF, dataset, type = "prob")[,"NonTumor"]
+        lift$SVM <- predict(SVM, dataset, type = "prob")[,"NonTumor"]
+        lift$NNET <- predict(NNET, dataset, type = "prob")[,"NonTumor"]
+      
+	cal_obj <- calibration(group ~ logisreg + elastnet + RF+SVM+NNET, data = lift,cuts = 10)
+        
 	ggplot(cal_obj, type = "l", auto.key = list(columns = 3, lines = T, points = F)) +
 	theme_bw() + theme(legend.position = "top") +
 	labs(title = "Calibration Curves of Models on dataset") +
