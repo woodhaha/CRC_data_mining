d43480b17471671ce36b0da1fe241a3e4810aa18
ZhouZhuha <woodhaha@gmail.com>
2019-06-25 14:25:37 +0800

    Update Cindex.R

--- Cindex.R
+++ Cindex.R
@@ -5,9 +5,10 @@ Cindex <- function(dataset) {
 	require(riskRegression)
 	registerDoMC()
 	cdata = dataset[, colnames(dataset) %in% c(cox_vip_list, "stage", "OS", "status")]
-	Models <- list("CoxPH.stage" = coxph(Surv(OS, status) ~stage, data = cdata, x = T, y = T) ,
-				"CoxPH" = coxph(Surv(OS, status) ~., data =cdata, x = T, y = T),
-	            "RandomForest" = rfsrc)
+	Models <- list(
+		      "CoxPH.stage" = coxph(Surv(OS, status) ~stage, data = cdata, x = T, y = T) ,
+		      "CoxPH" = coxph(Surv(OS, status) ~., data =cdata, x = T, y = T),
+	             "RandomForest" = rfsrc)
 	Cimp=pec::cindex(Models, formula = Hist(OS, status)~., data = cdata,eval.times = (seq(0, 3650, 30)),
 	splitMethod = "bootcv", M = round(nrow(cdata)*0.6), cens.model = "marginal",B=100,maxtime = 3000 )
 	return(Cimp)
